0cd70a434e9e50031544a5713b6560d9c694d9d1
Nguyen Tuan Kiet <nguyentuankiet23031998@gmail.com>
2017-02-05 15:14:15 +0700

    Create a http server

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-console.log("Hello World");
-var storage=require("node-persist");
-var yargs=require("yargs");
-var argv=yargs.argv;
-var isPrime=require("./checkPrime");
-console.log(isPrime.ktsnt(argv.n));
+var http = require("http");
+http.createServer(function (req, res) {
+    console.log("Hello World");
+    var storage = require("node-persist");
+    var yargs = require("yargs");
+    var argv = yargs.argv;
+    var isPrime = require("./checkPrime");
+    console.log(isPrime.ktsnt(argv.n));
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+}).listen(3000, "127.0.0.1");
