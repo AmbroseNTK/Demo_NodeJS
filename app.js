29b6afd13d12655f5f918698e6a033e2c40406b8
Nguyen Tuan Kiet <nguyentuankiet23031998@gmail.com>
2017-02-05 13:32:24 +0700

    Add module yargs

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 console.log("Hello World");
-var storage=require("node-persist")
-storage.initSync();
+var storage=require("node-persist");
+var yargs=require("yargs");
+var argv=yargs.argv;
+var isPrime=require("./checkPrime");
+console.log(isPrime.ktsnt(argv.n));
